befa817a7a3aada974621ddf73e29d404a3b64b3
aletex1994 <alessandrotezzavr@gmail.com>
2022-05-10 10:15:22 +0200

    added React to the frontend

--- src/declarations/ic_calculator/ic_calculator.did.js
+++ src/declarations/ic_calculator/ic_calculator.did.js
@@ -1,4 +1,16 @@
 export const idlFactory = ({ IDL }) => {
-  return IDL.Service({ 'greet' : IDL.Func([IDL.Text], [IDL.Text], []) });
+  const Name = IDL.Text;
+  const Phone = IDL.Nat;
+  const Entry = IDL.Record({
+    'name' : Name,
+    'email' : IDL.Text,
+    'phone' : Phone,
+    'address1' : IDL.Text,
+    'address2' : IDL.Text,
+  });
+  return IDL.Service({
+    'insert' : IDL.Func([Name, IDL.Text, IDL.Text, IDL.Text, Phone], [], []),
+    'lookup' : IDL.Func([Name], [IDL.Opt(Entry)], ['query']),
+  });
 };
 export const init = ({ IDL }) => { return []; };
