794e01749c5401b11fd25da8774db12c32a7783e
aletex1994 <alessandrotezzavr@gmail.com>
2022-05-10 11:09:11 +0200

    had an issue with babel-loader - solved

--- webpack.config.js
+++ webpack.config.js
@@ -94,6 +94,16 @@ module.exports = {
         ],
       },
       {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
